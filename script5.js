b74329018eeed2e6fc069ba26669f49525b65029
unknown <jeniltanti98257@gmail.com>
2025-03-11 15:45:32 +0530

    5

---
+++ script5.js
@@ -0,0 +1,14 @@
+//  JS array
+
+//  It is a general practis to use the `const` keyword to declare arrays
+
+//  JS array datatype are diffrent from usal array Datastucter.
+//  In more then weys, it is closer to python lists datatype
+
+const students = ['jenil','swami', 23, 21];
+
+console.log(students);
+
+console.log(students[1]);
+
+console.log(students.at(-2));
